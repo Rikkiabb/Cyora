ccfe0ba5bbc6ad4cc96f23caff05f2e23f802500
Stefán <stefanh13@ru.is>
2015-05-02 22:44:36 +0000

    Created key and animation

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Key : MonoBehaviour {
+
+	// Update is called once per frame
+	void OnTriggerEnter2D(Collider2D obj) {
+		if (obj.name == "Player") {
+			Destroy(gameObject);
+		}
+	}
+}
