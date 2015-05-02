28345e4216debb746ce659ece22b7d86353e03b5
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-02 11:22:40 +0000

    PlayerJump

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,25 +3,42 @@ using System.Collections;
 
 public class PlayerScript : MonoBehaviour {
 
+	// For left and right movement
 	public float maxSpeed = 10f;
 	Animator anim;
 
-	
-	// Use this for initialization
+	// Jump variables
+	bool grounded = false;
+	public Transform groundCheck;
+	float groundRadius = 0.2f;
+	public LayerMask whatIsGround;
+	public float jumpForce = 700f;
+
 	void Start () {
 		anim = GetComponent<Animator> ();
 		
 	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-		
-		float move = Input.GetAxis ("Horizontal");
-		
-		anim.SetFloat ("Speed", Mathf.Abs (move));
-		
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-		
+	void Update(){
+
+		// If player is on ground and space(jump) is pushed then we can jump
+		if (grounded && Input.GetButtonDown ("Jump")) {
+
+			anim.SetBool("Ground", false);
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce));
+		}
+	}
+
+	void FixedUpdate () {
+
+		// Check if we are on the ground
+		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
+		anim.SetBool ("Ground", grounded);
+
+		// Left and right movement
+		float move = Input.GetAxis ("Horizontal");
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+			
 	}
 }
