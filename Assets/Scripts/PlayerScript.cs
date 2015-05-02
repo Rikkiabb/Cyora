0cd5d6a1919b15ba79a3536a4b757e6c0178bca8
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-02 21:55:47 +0000

    Player jump and run artwork

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,9 @@ public class PlayerScript : MonoBehaviour {
 	public float maxSpeed = 10f;
 	Animator anim;
 
+	// Players running to right(true) or left(false)
+	bool facingRight = true;
+
 	// Jump variables
 	bool grounded = false;
 	public Transform groundCheck;
@@ -40,5 +43,20 @@ public class PlayerScript : MonoBehaviour {
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 			
+		// If player is moving(in a positive axis) and not facing right then we flip
+		if (move > 0 && !facingRight)
+			Flip ();
+		// If player is moving(in a negative axis) and not facing left then we flip
+		else if (move < 0 && facingRight)
+			Flip ();
+	}
+
+	void Flip(){
+
+		// Change the direction and flip the world
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
