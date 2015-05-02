7e2ceaaa106d892cc56eaa73638447c2db32cf2a
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-02 10:49:15 +0000

    Player move

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public float maxSpeed = 10f;
+	Animator anim;
+
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		float move = Input.GetAxis ("Horizontal");
+		
+		anim.SetFloat ("Speed", Mathf.Abs (move));
+		
+		GetComponent<Rigidbody2D>().velocity = new Vector2 (move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+
+		
+	}
+}
