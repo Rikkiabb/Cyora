372389960cd2f8145f490c9249a6db7342b3fbf5
Stefán <stefanh13@ru.is>
2015-05-02 23:19:27 +0000

    Added particles whith key collision

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Key : MonoBehaviour {
 
+	public Transform keyEffect;
+
 	// Update is called once per frame
 	void OnTriggerEnter2D(Collider2D obj) {
 		if (obj.name == "Player") {
+			Instantiate(keyEffect, transform.position, transform.rotation);
 			Destroy(gameObject);
 		}
 	}
