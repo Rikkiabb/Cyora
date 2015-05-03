ee476a2a8b2c9408519de67c3dafdbb188d5d4ff
Stefán <stefanh13@ru.is>
2015-05-03 18:33:11 +0000

    Camera stops following player when he finishes level

--- Assets/Scripts/CameraPosition.js
+++ Assets/Scripts/CameraPosition.js
@@ -1,12 +1,17 @@
 ﻿#pragma strict
 
 // camera follows player on x-axis so target is player
-var target : Transform;
+var realTarget : Transform;
+static var target : Transform;
 
+function Start(){
+	target = realTarget;
+}
 
 function Update () {
 	if(target != null){
 		// target.position is the position of the player, we add 0 to the x-axis, nothing to the y-axis and the z-axis is a constant
 		transform.position = target.position + Vector3(0, -target.position.y, -10);
+		realTarget = target;
 	}
 }
