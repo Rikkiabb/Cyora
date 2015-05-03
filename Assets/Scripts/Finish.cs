eead34b10bcdb4b34abcbd6876b600404af8fc20
Stefán <stefanh13@ru.is>
2015-05-03 17:22:28 +0000

    Created object to check if player finsished level

---
+++ Assets/Scripts/Finish.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D obj){
+
+		if (obj.name == "Player") {
+			Debug.Log("Player has finished!");
+		}
+	}
+}
