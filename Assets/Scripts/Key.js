5ff8a7e4291c726a9e6c1a1014b20473ade320fc
Stefán <stefanh13@ru.is>
2015-05-03 12:30:28 +0000

    Take two key collecting

---
+++ Assets/Scripts/Key.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var keyEffect : Transform;
+
+function OnTriggerEnter2D (obj : Collider2D){
+	
+	if(obj.name == "Player"){
+		
+		GameMaster.numbKeys++;
+		var effect = Instantiate(keyEffect, transform.position, transform.rotation);
+		Destroy(effect.gameObject, 2);
+		Destroy(gameObject);
+	}
+
+}
