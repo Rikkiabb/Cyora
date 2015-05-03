6e88df4470ae029efac99cc84f66f62072acb0f8
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-03 12:22:11 +0000

    Functionality added to main menu

---
+++ Assets/Scripts/MainMenu.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+
+function Quit(){
+	//Does nothing in unity but works when running on a operating system
+	Debug.Log("You just hit quit");
+	Application.Quit();
+}
+
+function NewGame(){
+	Application.LoadLevel("First");
+}
