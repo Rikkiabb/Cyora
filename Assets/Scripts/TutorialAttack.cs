3414d7249672f843a8a0aa9decd0f749a02a0220
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-04 18:42:00 +0000

    Added attack movement to tutorial

---
+++ Assets/Scripts/TutorialAttack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialAttack : MonoBehaviour {
+
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	
+	void Update () {
+		if (Input.GetButtonDown ("Fire1") && !anim.GetBool ("isKnight3Attacking")) {
+			
+			anim.SetBool("isKnight3Attacking", true);
+			StartCoroutine (waitAttack ());
+		}
+
+	}
+
+	IEnumerator waitAttack(){
+		
+		yield return new WaitForSeconds (0.5f);
+		
+		anim.SetBool("isKnight3Attacking", false);
+	}
+}
