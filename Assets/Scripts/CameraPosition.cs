c7f43b39ccf01366a50747ec0414da1f6f56ce36
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-05 19:07:05 +0000

    First level

--- Assets/Scripts/CameraPosition.cs
+++ Assets/Scripts/CameraPosition.cs
@@ -4,8 +4,7 @@ using System.Collections;
 public class CameraPosition : MonoBehaviour {
 	public Transform realTarget;
 	public static Transform target;
-	bool movedUp = false;
-	int i = 0;
+
 	
 	void Start(){
 		target = realTarget;
@@ -13,15 +12,16 @@ public class CameraPosition : MonoBehaviour {
 	
 	void Update () {
 		if(target != null){
-			if(target.position.y < 10){
-				// target.position is the position of the player, we add 0 to the x-axis, nothing to the y-axis and the z-axis is a constant
-				transform.position = target.position + new Vector3(0, -target.position.y, -10);
-			} else if(!movedUp) {
-				for(; i < 21; i++){
-					StartCoroutine (waitMove ());
-				}
+
+			// target.position is the position of the player, we add 0 to the x-axis, nothing to the y-axis and the z-axis is a constant
+			transform.position = target.position + new Vector3(0, 0, -10);
+		
+//			} else if(!movedUp) {
+//				for(; i < 21; i++){
+//					StartCoroutine (waitMove ());
+//				}
 //				movedUp = true;
-			}
+//			}
 //			} else {
 //				transform.position = target.position + new Vector3(0, -target.position.y + 20, -10);
 //			}
@@ -29,10 +29,10 @@ public class CameraPosition : MonoBehaviour {
 		}
 	}
 
-	IEnumerator waitMove(){
-		yield return new WaitForSeconds (20);
-		transform.position = target.position + new Vector3(0, -target.position.y + i, -10);
-	}
+//	IEnumerator waitMove(){
+//		yield return new WaitForSeconds (20);
+//		transform.position = target.position + new Vector3(0, -target.position.y + i, -10);
+//	}
 	
 	
 }
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovingPlatform : MonoBehaviour {
+	
+	public bool forward = false;
+	public float speed = 3f;
+	public bool leftToRight = true;
+	int timer = 0;
+	public int maxTimer = 10;
+	
+
+	// Update is called once per frame
+	void Update () {
+		
+		
+		if (forward) {
+			
+			//			if (leftToRight) {
+			//GetComponent<Rigidbody2D> ().velocity.x = speed;
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (speed, 0) ;
+			
+			//			rig.velocity.x = speed;
+			
+			//			} else {
+			//				//GetComponent<Rigidbody2D> ().velocity.y = speed;
+			//				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, speed));
+			//			}
+		} else {
+			
+			//			if (leftToRight) {
+			//GetComponent<Rigidbody2D> ().velocity.x = -speed;
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-speed, 0) ;
+			//			} else {
+			//				//GetComponent<Rigidbody2D> ().velocity.y = -speed;
+			//				GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, -speed));
+			//			}
+		}
+		
+		
+		timer++;
+		if (timer == maxTimer) {
+			timer = 0;
+			forward = !forward;
+		}
+		
+		
+	}
+}
