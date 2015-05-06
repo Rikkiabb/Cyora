522db9fd012bf314684a5f521fa72cfd9b884ca7
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-06 19:57:04 +0000

    Building level 2

---
+++ Assets/Scripts/LevelTwoMaster.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTwoMaster : MonoBehaviour {
+
+	bool windLeft, windRight;
+	int placement;
+
+	void Start(){
+		windLeft = false;
+		windRight = false;
+		placement = 0;
+	}
+
+
+	void OnTriggerEnter2D (Collider2D obj){
+		if (obj.name == "Player") {
+		
+			if(!windLeft && placement == 0){
+				windLeft = true;
+				placement++;
+				Physics2D.gravity = new Vector2(-115, -30);
+			}
+
+			if(!windRight && placement == 1){
+				windRight = true;
+				placement++;
+				Physics2D.gravity = new Vector2(115, -30);
+				GameObject explode = GameObject.FindGameObjectWithTag("Explode");
+				Destroy(explode);
+			}
+		
+		}
+	}
+}
