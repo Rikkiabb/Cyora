b925e9c42bc1980f0a7a6df9e81c052619d7dffe
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-11 10:53:05 +0000

    Upgrades resets at main menu

---
+++ Assets/Scripts/StartMaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+
+public class StartMaster : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		// Original settings of player
+		BinaryFormatter bf = new BinaryFormatter();
+		FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+		PlayerData data = new PlayerData();
+		data.heal = 3;
+		data.jf = 1410;
+		data.hdj = false;
+		data.swordSizeX = 1.3f;
+		data.swordSizeY = 1.3f;
+		bf.Serialize(file, data);
+		file.Close ();
+	}
+
+}
