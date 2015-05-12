c65adf40537c6865413213af1cbdb5b2b8ecc85c
gunnargylfa <gunnargylfa@gmail.com>
2015-05-12 22:51:33 +0000

    audio additions

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -5,9 +5,14 @@ public class Key : MonoBehaviour {
 
 	public Transform keyEffect;
 
+	//the source file for the audio effect
+	public AudioClip keyCollect;
+	private AudioSource source;
+
 	//To trigger dissapearing
 	public bool dissapear = false;
 	
+
 	void OnTriggerEnter2D (Collider2D obj){
 		
 		if(obj.name == "Player"){
@@ -15,13 +20,14 @@ public class Key : MonoBehaviour {
 
 			Instantiate(keyEffect, transform.position, transform.rotation);
 			ScoreManager.numbKeys++;
-
-
+			source = GetComponent<AudioSource>();
+			// play the sound effect
+			source.PlayOneShot(keyCollect, 1F);		
 			GameObject effect2 = GameObject.FindGameObjectWithTag("KeyEffect");
 			Destroy(effect2, 2);
 //			Destroy(gameObject);
 			gameObject.SetActive(false);
-	
+		
 
 			if(dissapear){
 				GameObject explode = GameObject.FindGameObjectWithTag("Explode");	
