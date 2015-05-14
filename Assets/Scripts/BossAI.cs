0ec021daff318df78a54e295662c14bbd7dcca17
Stefán <stefanh13@ru.is>
2015-05-14 16:02:03 +0000

    Story is finished

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -129,13 +129,17 @@ public class BossAI : MonoBehaviour {
 			game.StopFinal("stage4");
 			Instantiate(heart, new Vector3(-304.4326f, 893.6852f, 0), Quaternion.identity);
 		}
-		else if(stats.Health <= 0){
+		else if(stats.Health <= 11){
 			GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
 			
 			for (int i = 0; i < enemies.Length; i++) {
 				Destroy (enemies[i].gameObject);
 			}
+			GameMasterCS game = GameObject.Find("_GM").GetComponent<GameMasterCS>();
+			game.Victory();
 			Destroy (transform.parent.gameObject);
+
+
 			return;
 		}
 
@@ -225,7 +229,7 @@ public class BossAI : MonoBehaviour {
 		yield return new WaitForSeconds(2);
 		canShoot = true;
 	}
-
+	
 	public IEnumerator WaitHurt(){
 		yield return new WaitForSeconds(2);
 		isHurt = false;
