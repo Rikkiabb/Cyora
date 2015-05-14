1967a3fd5c87b49c204e55133b09023531a17793
Stefán <stefanh13@ru.is>
2015-05-14 16:25:36 +0000

    Fixed enemy movement in final battle

--- Assets/Scripts/BossAI.cs
+++ Assets/Scripts/BossAI.cs
@@ -129,7 +129,7 @@ public class BossAI : MonoBehaviour {
 			game.StopFinal("stage4");
 			Instantiate(heart, new Vector3(-304.4326f, 893.6852f, 0), Quaternion.identity);
 		}
-		else if(stats.Health <= 11){
+		else if(stats.Health <= 0){
 			GameObject[] enemies = GameObject.FindGameObjectsWithTag ("Enemy");
 			
 			for (int i = 0; i < enemies.Length; i++) {
