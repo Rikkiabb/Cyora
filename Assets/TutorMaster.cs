b06aece67689b94f6bb623aae87f39d433f1f195
Rikard Arnar B. Birgisson <rikardarnar@gmail.com>
2015-05-17 14:38:59 +0000

    Finish of Tutor

---
+++ Assets/TutorMaster.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorMaster : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (CanvasController.clearedLevel) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
