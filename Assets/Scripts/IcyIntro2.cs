7b308c13e41345e185b7596c4457f5d945456935
Stefán <stefanh13@ru.is>
2015-05-18 13:21:49 +0000

    Fix broken intro ice scene

--- Assets/Scripts/IcyIntro2.cs
+++ Assets/Scripts/IcyIntro2.cs
@@ -1,30 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class IcyIntro : MonoBehaviour {
-	
+public class IcyIntro2 : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
-		StartCoroutine (WaitText ());
-		
+		StartCoroutine (WaitScene ());
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
 		if (Input.GetButtonDown ("Fire1")) {
 			Application.LoadLevel("Icy");
 		}
-
-
 	}
-	
-	IEnumerator WaitText(){
-		
+
+	IEnumerator WaitScene(){
 		yield return new WaitForSeconds (11.7f);
-		Application.LoadLevel("IcyIntro2");
-		
+		Application.LoadLevel("Icy");
 	}
 }
